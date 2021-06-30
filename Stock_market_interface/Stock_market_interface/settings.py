dd72e30eb08af0bd8182aabef831bfce06c0fb81
OsterFar <56218868+OsterFar@users.noreply.github.com>
2021-06-30 13:11:22 +0500

    Update settings.py

--- Stock_market_interface/Stock_market_interface/settings.py
+++ Stock_market_interface/Stock_market_interface/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hq(rvc49b4@xr@u$lsiy#cnynud9vn#3k4aj8rg7e+#c-1^($4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
